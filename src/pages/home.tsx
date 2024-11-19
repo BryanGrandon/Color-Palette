ca664388bb17e231723703c367fdafa179b90686
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-11-19 19:22:58 -0300

    refactor(web):change home for examples

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,111 @@
 const Home = () => {
-  return <main className='home'>Home</main>
+  return (
+    <main className='home'>
+      <article className='article'>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+        <section className='card'>
+          <section className='card__header'>
+            <p>#ffff</p>
+          </section>
+          <section className='card__main'>
+            <p className='card__p'>C</p>
+            <p className='card__p'>S</p>
+            <p className='card__p'>L</p>
+            <p className='card__p'>R</p>
+          </section>
+        </section>
+      </article>
+    </main>
+  )
 }
 
 export default Home
