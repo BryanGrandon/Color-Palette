2487c4a15cba7fe19d12f3e7e47ca709594acb22
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-01-16 19:01:56 -0300

    feat(web):add button UI in core

---
+++ src/core/components/ui/button.tsx
@@ -0,0 +1,15 @@
+type Button = {
+  text: string
+  className?: string
+  onClick?: () => void
+}
+
+const Button = ({ text, className, onClick }: Button) => {
+  return (
+    <button className={`${className} button`} onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+export default Button
