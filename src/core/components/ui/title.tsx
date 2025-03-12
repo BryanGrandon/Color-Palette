b49981d7d2585764109c7c9990de7657d22f2a19
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-03-12 06:06:10 -0300

    test(web):add title in core

--- src/core/components/ui/title.tsx
+++ src/core/components/ui/title.tsx
@@ -1,9 +1,10 @@
 type Title = {
   text: string
+  className?: string
 }
 
-const Title = ({ text }: Title) => {
-  return <h2 className='title'>{text}</h2>
+const Title = ({ text, className = '' }: Title) => {
+  return <h2 className={`title ${className}`}>{text}</h2>
 }
 
 export default Title
