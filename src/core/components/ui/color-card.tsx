6b9127cbebacdaf947d9d13a46126e3c2827f3ef
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-03-17 13:51:26 -0300

    refactor(web):change files for problem solving

--- src/core/components/ui/color-card.tsx
+++ src/core/components/ui/color-card.tsx
@@ -5,7 +5,7 @@ type Color_Card = {
 }
 
 const ColorCard = ({ hex }: Color_Card) => {
-  const regExpConditional = /^#[A-F0-9]{3}$/.test(hex) || /^#[A-F0-9]{6}$/.test(hex)
+  const regExpConditional = /^#[A-Fa-f0-9]{3}$/.test(hex) || /^#[A-Fa-f0-9]{6}$/.test(hex)
   const color = regExpConditional ? hex : '#ffffff'
 
   return (
--- src/pages/pages-saved/components/ui/confirm-deletion.tsx
+++ src/pages/pages-saved/components/ui/confirm-deletion.tsx
@@ -7,20 +7,22 @@ type Confirm_Deletion = {
 const ConfirmDeletion = ({ onClick }: Confirm_Deletion) => {
   const handlerClick = (type: boolean) => onClick(type)
   return (
-    <article className='confirm-deletion'>
-      <section className='confirm-deletion__icon'>
-        <CiWarning />
-      </section>
-      <section className='confirm-deletion__content'>
-        <section>
-          <h2 className='confirm-deletion__title'>Warning</h2>
-          <p>Do you wish to delete the color palette?</p>
+    <article className='notify-card'>
+      <article className='confirm-deletion'>
+        <section className='confirm-deletion__icon'>
+          <CiWarning />
         </section>
-        <section className='confirm-deletion__buttons'>
-          <Button text='Cancel' className='button--cancel' onClick={() => handlerClick(false)} />
-          <Button text='Delete' className='button--delete' onClick={() => handlerClick(true)} />
+        <section className='confirm-deletion__content'>
+          <section>
+            <h2 className='confirm-deletion__title'>Warning</h2>
+            <p>Do you wish to delete the color palette?</p>
+          </section>
+          <section className='confirm-deletion__buttons'>
+            <Button text='Cancel' className='button--cancel' onClick={() => handlerClick(false)} />
+            <Button text='Delete' className='button--delete' onClick={() => handlerClick(true)} />
+          </section>
         </section>
-      </section>
+      </article>
     </article>
   )
 }
